93173c07e75e72766dfbfb9733bb375656efd37c
seomini <seomini970204@gmail.com>
2024-02-28 15:33:02 +0900

    여러개의 변수를 만드는법

---
+++ study_swift/MuCollection.swift
@@ -0,0 +1,18 @@
+//
+//  MuCollection.swift
+//  study_swift
+//
+//  Created by mimi_0_0 on 2024/02/28.
+//
+
+import SwiftUI
+
+struct MuCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MuCollection()
+}
