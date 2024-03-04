545e9819fa1e5babe236a888932585759c1c6fac
seomini <seomini970204@gmail.com>
2024-03-04 15:25:16 +0900

    클래스

--- study_swift/Preview Content/Diff.swift
+++ study_swift/Preview Content/Diff.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct Diff: View {
+    
+    let myCar = Car(name: "리어카", owner: "라이오")
+    let myKar = Kar(name:"리어카2", owner: "라이오2")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(myCar.name)의 주인은 \(myCar.owner)입니다.")
+            
+            Button {
+                
+                let broCar = myKar
+                broCar.name = "동생차"
+                broCar.owner = "동생"
+                
+                myKar.sayHi()
+            } label: {
+                    Text("출발")
+            }
+        }
+        
+    }
+}
+
+struct Car {
+    let name: String
+    let owner: String
+    
+    func sayHi(){
+        print("hi \(owner)")
+    }
+}
+
+class Kar {
+    var name: String
+    var owner: String
+    
+    func sayHi(){
+        print("hi \(owner)")
+    }
+    
+    init(name: String, owner: String) {
+        self.name = name
+        self.owner = owner
     }
 }
 
